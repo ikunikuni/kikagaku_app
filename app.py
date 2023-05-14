4f3fa1b0badb59a3486dcc6dbdefb37a8cde6952
kuni <kuni@kuninoMacBook-Air.local>
2023-05-14 13:12:32 +0900

    15sh commmit

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ from io import BytesIO
 import pyocr
 import re
 import datetime
+import easyocr
+import numpy as np
+import pytesseract
+from mynum import detect_gender
+from mynum import detect_birthdate
 
 def predict(img):
     net = Net().cpu().eval()
@@ -71,68 +76,25 @@ def home():
             #base64_str = base64.b64encode(buf.getvalue()).decode('UTF-8')
             #base64_data = 'data:image/png;base64,{}'.format(base64_str)
 
-        image_data_url = request.form['imageData']
-        image_data = re.sub('^data:image/.+;base64,', '', image_data_url)
-        image = Image.open(BytesIO(base64.b64decode(image_data)))
+        image_data_url = request.form['imageData'] #cam
+        image_data = re.sub('^data:image/.+;base64,', '', image_data_url) #cam
+        image = Image.open(BytesIO(base64.b64decode(image_data))) #cam
 
         pred = predict(image)
         seibetsuDanjyo_ = getDanjyo(pred)
         #return render_template('result.html', seibetsuDanjyo=seibetsuDanjyo_, image=base64_data)
-        return render_template('result.html', seibetsuDanjyo=seibetsuDanjyo_, image=image_data_url)
-        return redirect(request.url)
+        return render_template('result.html', seibetsuDanjyo=seibetsuDanjyo_, image=image_data_url) #cam
+        #return redirect(request.url)
 
     elif request.method == 'GET':
         return render_template('webcam.html')
+        #return render_template('index.html')
     
-
-
-def detect_gender(txt):
-    if '男' in txt:
-        gender = '男'
-    elif '女' in txt:
-        gender = '女'
-    else:
-        gender = '不明'
-    return gender
-
-
-def detect_birthdate(txt):
-    birthdate_regex = r'(平成|昭和|大正|明治|令和)(\d+|[元一二三四五六七八九十]+)年\s+(\d+月\d+日)生'
-    birthdate_match = re.search(birthdate_regex, txt)
-    if birthdate_match:
-        era = birthdate_match.group(1)
-        year = birthdate_match.group(2)
-        if year == "元":
-            year = "1"
-
-        if era == "平成":
-            year_number = int(year) + 1988
-            date = str(year_number) + '年' + birthdate_match.group(3)  # 年数 + 年月日
-        elif era == "昭和":
-            year_number = int(year) + 1925
-            date = str(year_number) + '年' + birthdate_match.group(3)  # 年数 + 年月日
-        elif era == "大正":
-            year_number = int(year) + 1911
-            date = str(year_number) + '年' + birthdate_match.group(3)  # 年数 + 年月日
-        elif era == "明治":
-            year_number = int(year) + 1867
-            date = str(year_number) + '年' + birthdate_match.group(3)  # 年数 + 年月日
-        elif era == "令和":
-            year_number = int(year) + 2018
-            date = str(year_number) + '年' + birthdate_match.group(3)  # 年数 + 年月日
-        else:
-            date = era + year + '年' + birthdate_match.group(3)  # 元号 + 年数 + 年月日
-    
-        
-        birth_day = datetime.datetime.strptime(date, '%Y年%m月%d日')
-        current_date = datetime.datetime.now()
-        age = current_date.year - birth_day.year - ((current_date.month, current_date.day) < (birth_day.month, birth_day.day))
-        return age
-    else:
-        return None  
+#######################################################################################################
 
 
 # OCRツールの初期化
+reader = easyocr.Reader(['ja'])
 tools = pyocr.get_available_tools()
 tool = tools[0]
 
@@ -140,21 +102,31 @@ tool = tools[0]
 def mynum():
     if request.method == 'POST':
         # リクエストから画像ファイルを取得
-        file2 = request.files['filename2']
-        # PIL.Imageに変換
-        img2 = Image.open(file2)
-        # 画像からテキストを抽出
-        txt = tool.image_to_string(img2, lang='jpn+eng',builder=pyocr.builders.TextBuilder(tesseract_layout=6))
-        # 抽出したテキストから性別を判定
-        gender = detect_gender(txt)
-        if gender == '男':
-            gender = 'male'
-        elif gender == '女':
-            gender = 'female'
-        else:
-            gender = 'unknown'
-        # 抽出したテキストから年齢を判定
-        age = detect_birthdate(txt)
+        img_file = request.files['filename2']
+        # OpenCVを使用して画像を読み込む
+        img_bytes = img_file.read()
+        img_np = np.frombuffer(img_bytes, np.uint8)
+        img_gen = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
+
+        # EasyOCRを使用して性別を抽出
+        img_gray = cv2.cvtColor(img_gen, cv2.COLOR_BGR2GRAY)
+        #result_gender = reader.readtext(img_gray)
+        txt_gender = reader.readtext(img_gray)
+        #txt_gender = result_gender[6][1]
+
+        #print(result_gender)
+        #print(txt_gender)
+
+
+        gender = detect_gender(txt_gender)
+
+        # PyOCRを使用して生年月日を抽出
+        img_pil = Image.open(io.BytesIO(img_bytes))
+        img_pil = img_pil.convert('L')
+        txt_age = tool.image_to_string(img_pil, lang='jpn+eng', builder=pyocr.builders.TextBuilder(tesseract_layout=6))
+
+        age = detect_birthdate(txt_age)
+
         # 結果をレンダリングするテンプレートに渡してレスポンスを返す
         return render_template('result_mynum.html', gender=gender, age=age)
     else:
--- mynum.py
+++ mynum.py
@@ -3,29 +3,41 @@ import pyocr.builders
 import re
 import datetime
 from PIL import Image
+import easyocr
+import cv2
+import numpy as np
 
 
-def detect_gender(txt):
-    
-    # 正規表現にマッチする部分を検索
-    gender_regex = r'^性別\s+(.+)$'
-    gender_match = re.search(gender_regex, txt, re.MULTILINE)
-    
-    # マッチしたグループを抽出
-    if gender_match:
-        gender = gender_match.group(1)
-    else:
-        gender = "unknown"
-    
-    return gender
+def detect_gender(txt_gender):
+    # 性別を検索する文字列
+    gender_match = "性別"
+
+    for result in txt_gender:
+        # テキストと確信度の取得
+        text = result[1]
+        confidence = result[2]
+
+        # "性別"がテキストに含まれる場合
+        if gender_match in text:
+            # "性別"の直後にあるテキストを取得
+            index = text.find(gender_match)
+            gender = text[index+len(gender_match):].strip()
+
+            if '男' in gender:
+                return 'male'
+            elif '女' in gender:
+                return 'female'
+            else:
+                return 'unknown'
+            
 
 
 # 生年月日の抽出
-def detect_birthdate(txt):
+def detect_birthdate(txt_age):
     # 正規表現パターンの定義
     birthdate_regex = r'(平成|昭和|大正|明治|令和)(\d+|[元一二三四五六七八九十]+)年\s+(\d+月\d+日)生'
     # 正規表現にマッチする部分を検索
-    birthdate_match = re.search(birthdate_regex, txt)
+    birthdate_match = re.search(birthdate_regex, txt_age)
     # マッチした部分があれば生年月日を取得
     if birthdate_match:
         era = birthdate_match.group(1)
