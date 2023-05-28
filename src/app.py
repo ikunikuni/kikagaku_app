724bb1f75faa919f24cdf0850a143c58386d5476
kuni <kuni@kuninoMacBook-Air.local>
2023-05-28 15:15:49 +0900

    35th commmit

--- src/app.py
+++ src/app.py
@@ -4,23 +4,21 @@ from flask import Flask, request, render_template, redirect
 import io
 from PIL import Image
 import base64
-#import cv2
+import cv2
 from flask import Response
 from torchvision import transforms
-#import pytesseract
+import pytesseract
 from io import BytesIO
-#import pyocr
+import pyocr
 import re
-#import datetime
-#import easyocr
-#import numpy as np
-#from mynum import detect_gender
-#from mynum import detect_birthdate
-#import os
+import datetime
+import easyocr
+import numpy as np
+from mynum import detect_gender
+from mynum import detect_birthdate
+import os
 from flask import url_for
-#from webcam import VideoCamera
-#from segm import perform_segmentation
-#from segm import convert_image_data
+
 
 
 def predict(img):
@@ -48,93 +46,54 @@ def getDanjyo(label):
 
 app = Flask(__name__)
 
+camera = cv2.VideoCapture(0)
+
+face_cascade_path = 'haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(face_cascade_path)
 
 
-#def gen_frames():
-    #while True:
-        #frame = cv2.get_frame()
-        #yield (b'--frame\r\n'
-               #b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-        
-#def gen_frames(camera):
-    # カメラからフレームを取得するコード
-    #frame = camera.get_frame()
-    #yield (b'--frame\r\n'
-           #b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-def gen(camera): #f
+def gen(camera):
     while True:
-        frame = camera.get_frame()
-        if frame is not None:
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        ret, frame = camera.read()  # フレームを取得
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-#@app.route('/video_feed')
-#def video_feed():
-    #return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        
+        frame_with_faces = frame.copy()
 
+        frame_with_faces = cv2.cvtColor(frame_with_faces, cv2.COLOR_BGR2RGB)
+        
 
+    # フレームをバイナリデータに変換
+        ret, jpeg = cv2.imencode('.jpeg', frame_with_faces)
+        frame_bytes = jpeg.tobytes()
 
-
-#ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif', 'jpeg'])
-
-#def allowed_file(filename):
-    #return ('.') in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-
-
-        #ret, buffer = cv2.imencode('.jpg', frame) #?
-        #frame = buffer.tobytes() #?
-        #frame_bytes = buffer.tobytes() #?
-
-        #yield (b'--frame\r\n' #?
-               #b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') #?
-               #b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n') #?
-
-    #cap.release()
-
-# 画像セグメンテーションのエンドポイント
-#@app.route('/segment', methods=['POST'])
-#def segment_image():
-    # リクエストから画像データを取得
-    #s_image_data = request.form['imageData']
-    
-    # 画像データを復元しセグメンテーション処理を実行
-    #s_image = convert_image_data(s_image_data)
-    #segmented_image = perform_segmentation(s_image)
-    
-    # セグメンテーション結果をJSON形式でレスポンス
-    #return {'segmented_image': segmented_image}
-
-
-
-# セグメンテーション後のデータを受け取るエンドポイント
-@app.route('/result', methods=['POST'])
-def receive_segmented_data():
-    segmented_data = request.json['result']
-    
-    return 'imageData'
+    # フレームデータをジェネレータとして返す
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n\r\n')
 
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        image_data_url = request.form['imageData'] #cam
-        image_data = re.sub('^data:image/.+;base64,', '', image_data_url) #cam
-        image = Image.open(BytesIO(base64.b64decode(image_data))) #cam
+        image_data_url = request.form['imageData'] 
+        image_data = re.sub('^data:image/.+;base64,', '', image_data_url) 
+        image = Image.open(BytesIO(base64.b64decode(image_data))) 
 
         pred = predict(image)
         seibetsuDanjyo_ = getDanjyo(pred)
-        return render_template('result.html', seibetsuDanjyo=seibetsuDanjyo_, image=image_data_url) #cam
+        return render_template('result.html', seibetsuDanjyo=seibetsuDanjyo_, image=image_data_url) 
 
     elif request.method == 'GET':
-        #return render_template('f_webcam.html')
-        #return render_template('s_webcam.html')
         return render_template('webcam.html')
 
     
@@ -142,44 +101,41 @@ def home():
 
 
 # OCRツールの初期化
-#reader = easyocr.Reader(['ja'])
-#tools = pyocr.get_available_tools()
-#tool = tools[0]
+reader = easyocr.Reader(['ja'])
+tools = pyocr.get_available_tools()
+tool = tools[0]
 
-#@app.route('/mynum', methods=['GET', 'POST'])
-#def mynum():
-    #if request.method == 'POST':
+@app.route('/mynum', methods=['GET', 'POST'])
+def mynum():
+    if request.method == 'POST':
         # リクエストから画像ファイルを取得
-        #img_file = request.files['filename2']
+        img_file = request.files['filename2']
         # OpenCVを使用して画像を読み込む
-        #img_bytes = img_file.read()
-        #img_np = np.frombuffer(img_bytes, np.uint8)
-        #img_gen = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
+        img_bytes = img_file.read()
+        img_np = np.frombuffer(img_bytes, np.uint8)
+        img_gen = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
 
         # EasyOCRを使用して性別を抽出
-        #img_gray = cv2.cvtColor(img_gen, cv2.COLOR_BGR2GRAY)
-        #txt_gender = reader.readtext(img_gray)
+        img_gray = cv2.cvtColor(img_gen, cv2.COLOR_BGR2GRAY)
+        txt_gender = reader.readtext(img_gray)
 
 
 
-        #gender = detect_gender(txt_gender)
+        gender = detect_gender(txt_gender)
 
         # PyOCRを使用して生年月日を抽出
-        #img_pil = Image.open(io.BytesIO(img_bytes))
-        #img_pil = img_pil.convert('L')
-        #txt_age = tool.image_to_string(img_pil, lang='jpn+eng', builder=pyocr.builders.TextBuilder(tesseract_layout=6))
+        img_pil = Image.open(io.BytesIO(img_bytes))
+        img_pil = img_pil.convert('L')
+        txt_age = tool.image_to_string(img_pil, lang='jpn+eng', builder=pyocr.builders.TextBuilder(tesseract_layout=6))
 
-        #age = detect_birthdate(txt_age)
+        age = detect_birthdate(txt_age)
 
         # 結果をレンダリングするテンプレートに渡してレスポンスを返す
-        #return render_template('result_mynum.html', gender=gender, age=age)
-    #else:
-        #return render_template('mynum.html')
+        return render_template('result_mynum.html', gender=gender, age=age)
+    else:
+        return render_template('mynum.html')
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-#if __name__ == '__main__':
-    #app.run(host='0.0.0.0', debug=True)
